a0adedb0d1d8de7b12d3fec066921c1fcfcc046c
LiuGangjian <arlenliugj@sina.cn>
2018-09-20 16:57:28 +0800

    评价页

--- CarHelper/MineModule/View/GJMineEvaluateTopCell.m
+++ CarHelper/MineModule/View/GJMineEvaluateTopCell.m
@@ -7,10 +7,12 @@
 //
 
 #import "GJMineEvaluateTopCell.h"
+#import "XHStarRateView.h"
 
-@interface GJMineEvaluateTopCell ()
+@interface GJMineEvaluateTopCell () <XHStarRateViewDelegate>
 @property (nonatomic, strong) UILabel *titleLB;
 @property (nonatomic, strong) UIView *midLine;
+@property (nonatomic, strong) XHStarRateView *starView;
 @end
 
 @implementation GJMineEvaluateTopCell
@@ -37,16 +39,28 @@
     _midLine = [[UIView alloc] init];
     _midLine.backgroundColor = APP_CONFIG.appBackgroundColor;
     
+    CGFloat w = AdaptatSize(120);
+    CGFloat x = SCREEN_W / 2 - w / 2;
+    _starView = [[XHStarRateView alloc] initWithFrame:CGRectMake(x, 0, w, AdaptatSize(30))];
+    _starView.delegate = self;
+    
     [self.contentView addSubview:_titleLB];
     [self.contentView addSubview:_midLine];
+    [self.contentView addSubview:_starView];
+    [self addFiveBtns];
 }
 
 - (void)buttonClick:(UIButton *)btn {
     
 }
 
+- (void)starRateView:(XHStarRateView *)starRateView currentScore:(CGFloat)currentScore {
+    NSLog(@"%f", currentScore);
+}
+
 - (void)addFiveBtns {
     NSArray *btnTexts = @[@"差", @"一般", @"不错", @"很满意", @"推荐"];
+    CGFloat w = (SCREEN_W - 10 * (btnTexts.count + 2)) / btnTexts.count;
     [btnTexts enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         UIButton *btn = [[UIButton alloc] init];
         btn.titleLabel.font = [APP_CONFIG appAdaptFontOfSize:14];
@@ -58,7 +72,10 @@
         [btn addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
         [self.contentView addSubview:btn];
         [btn mas_makeConstraints:^(MASConstraintMaker *make) {
-            
+            make.top.equalTo(_midLine).with.offset(11);
+            make.height.mas_equalTo(AdaptatSize(35));
+            make.width.mas_equalTo(w);
+            make.left.equalTo(self).with.offset(10 + idx * (w + 10));
         }];
     }];
 }
@@ -74,6 +91,7 @@
         make.height.mas_equalTo(1);
         make.top.equalTo(_titleLB.mas_bottom).with.offset(AdaptatSize(15));
     }];
+    [_starView setY:self.height - AdaptatSize(65)];
 }
 
 - (CGFloat)height {
