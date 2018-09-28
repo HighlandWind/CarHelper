8d64727177b77ed6b8036e095daad8000afd4851
LiuGangjian <arlenliugj@sina.cn>
2018-09-28 18:02:10 +0800

    fix

---
+++ CarHelper/HomeModule/View/WashCar/GJWashCarListFilterView.h
@@ -0,0 +1,13 @@
+//
+//  GJWashCarListFilterView.h
+//  CarHelper
+//
+//  Created by hsrd on 2018/9/28.
+//  Copyright © 2018年 CAR. All rights reserved.
+//
+
+#import "GJBaseView.h"
+
+@interface GJWashCarListFilterView : GJBaseView
+
+@end
---
+++ CarHelper/HomeModule/View/WashCar/GJWashCarListFilterView.m
@@ -0,0 +1,22 @@
+//
+//  GJWashCarListFilterView.m
+//  CarHelper
+//
+//  Created by hsrd on 2018/9/28.
+//  Copyright © 2018年 CAR. All rights reserved.
+//
+
+#import "GJWashCarListFilterView.h"
+
+@implementation GJWashCarListFilterView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [UIColor greenColor];
+    }
+    return self;
+}
+
+@end
